ed5d2fa7c8334fea737bfa7f6999146c3892a6f1
JaileneJordan <jailene.jordan96@gmail.com>
2019-01-04 01:41:29 -0600

    mi parte del sistema

--- insertmateria.php
+++ insertmateria.php
@@ -1,14 +1,13 @@
 <?php
+    NuevoMateria($_POST['nombre']);
 
-  NuevoMateria($_POST['nombre']);
-
-  function NuevoMateria($nombre){
-    $conn=new mysqli('localhost', 'root', '', 'asesorias_successfull');
-    $sqlmateria = "INSERT INTO materia (id_materia, nombre) VALUES ('','".$nombre."')";
-    $conn->query($sqlmateria) or die("Error al ingresar los datos".mysqli_error($conn));
-    $conn->close();
-    header('Location: adminregistromaterias.php');
-  }
+    function NuevoMateria($nombre){
+      $conn=new mysqli('localhost', 'root', '', 'asesorias_successfull');
+      $sqlmateria = "INSERT INTO materia (id_materia, nombre) VALUES ('','".$nombre."')";
+      $conn->query($sqlmateria) or die("Error al ingresar los datos".mysqli_error($conn));
+      $conn->close();
+      header('Location: adminregistromaterias.php');
+    }
 
 
 ?>
